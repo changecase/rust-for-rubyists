226227244b9950c98da2173cbcbe5bc23cd381c3
changecase <jeff.ja@gmail.com>
2014-07-30 18:26:59 -0700

    create fizzbuzz

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    for num in range(1i, 101) {
+        println!("{:s}",
+            if div_by_fifteen(num){ "FizzBuzz".to_string() }
+            else if div_by_three(num) { "Fizz".to_string() }
+            else if div_by_five(num) { "Buzz".to_string() }
+            else { num.to_string() }
+        );
+    }
+}
+
+fn div_by_fifteen(num: int) -> bool {
+    num % 15 == 0
+}
+
+fn div_by_three(num: int) -> bool {
+    num % 3 == 0
+}
+
+fn div_by_five(num: int) -> bool {
+    num % 5 == 0
+}
+
+#[test]
+fn test_div_by_three() {
+    if div_by_three(1) {
+        fail!("One is not three");
+    }
+}
+
+#[test]
+fn test_div_by_three_with_three() {
+    assert!(div_by_three(3), "Three is three")
+}
+
+#[test]
+fn test_div_by_five_with_five() {
+    assert!(div_by_five(5), "Five is five")
+}
+
+#[test]
+fn test_div_by_fifteen_with_fifteen() {
+    assert!(div_by_fifteen(15), "Fifteen is fifteen")
+}
